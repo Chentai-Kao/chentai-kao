6f8b4c5f446c642947c8dcad0b2a2a3ff8092184
Cobra Kao <ru6xul6@gmail.com>
2014-03-29 16:14:14 -0700

    Fill data from JSON

--- js/index.js
+++ js/index.js
@@ -1,15 +1,32 @@
 $(function() {
 
-  $(".content-projects").hide();
-  $("#nav-about").click(function(e) {
-    e.preventDefault();
-    $(".content").hide();
-    $(".content-about").show();
-  });
-  $("#nav-projects").click(function(e) {
-    e.preventDefault();
-    $(".content").hide();
-    $(".content-projects").show();
-  });
+  // get data from server
+  function fillData() {
+    $.getJSON("/data/data.json", function(data) {
+      var template = $("#event-template");
+      function createEventDiv(e) {
+        var t = template.clone().removeClass("template");
+        t.find(".event-title").text(e.title);
+        t.find(".event-image").append($("<img>", { src: e.image }));
+        t.find(".event-description").text(e.description);
+        return t;
+      }
+      // "about" data
+      for (var i = 0; i < data.about.events.length; ++i) {
+        var e = data.about.events[i];
+        var t = createEventDiv(e);
+        $(".content-about").append(t);
+      }
+      // "projects" data
+      for (var i = 0; i < data.projects.length; ++i) {
+        var e = data.projects[i];
+        var t = createEventDiv(e);
+        $(".content-projects").append(t);
+      }
+    });
+  }
+
+  // main function
+  fillData();
 
 });
