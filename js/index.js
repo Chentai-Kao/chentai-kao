42ea7d7c6c179c727baa641c116d467f18e6b51d
Cobra Kao <ru6xul6@gmail.com>
2014-03-29 21:22:18 -0700

    Scroll spy

--- js/index.js
+++ js/index.js
@@ -1,16 +1,5 @@
 $(function() {
 
-  // animate scrolling of navigation bar
-  function navScroller() {
-    $(".nav a").click(function(e) {
-      e.preventDefault();
-      var targetSelector = $(this).attr("href");
-      var offset = parseInt($(targetSelector).offset().top);
-      var headerHeight = parseInt($(".header-container").height());
-      $("html, body").animate({ scrollTop: offset - headerHeight }, 300);
-    });
-  }
-
   // get data from server
   function fillData() {
     $.getJSON("/data/data.json", function(data) {
@@ -33,8 +22,48 @@ $(function() {
     });
   }
 
+  // animate scrolling of navigation bar
+  function navScroller() {
+    $(".nav a").click(function(e) {
+      e.preventDefault();
+      var targetSelector = $(this).attr("href");
+      var offset = parseInt($(targetSelector).offset().top);
+      var headerHeight = parseInt($(".header-container").height());
+      $("html, body").animate({ scrollTop: offset - headerHeight }, 300);
+    });
+  }
+
+  // Highlight navigation bar based on scroll position
+  function scrollSpy() {
+    $(document).scroll(function(e) {
+      // find ID of the target div
+      var headerHeight = parseInt($(".header-container").height());
+      var boundaryPos = parseInt($(document).scrollTop()) + headerHeight;
+      var minDistance = parseInt($(document).height());
+      var targetId = $(".content").first().attr("id");
+      $(".content-container").each(function() {
+        var divPos = parseInt($(this).offset().top);
+        var distance = divPos - boundaryPos;
+        if (distance <= 0 && Math.abs(distance) < minDistance) {
+          minDistance = Math.abs(distance);
+          targetId = $(this).find(".content").attr("id");
+        }
+      });
+      // use the target ID to backtrace the navigation bar
+      $(".nav a").each(function() {
+        if ($(this).attr("href") === "#" + targetId) {
+          $(this).addClass("highlight");
+        }
+        else {
+          $(this).removeClass("highlight");
+        }
+      });
+    });
+  }
+
   // main function
   fillData();
   navScroller();
+  scrollSpy();
 
 });
