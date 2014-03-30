9078d14ea5917c568b5404143db9f00c65266c14
Cobra Kao <ru6xul6@gmail.com>
2014-03-29 17:04:47 -0700

    Add section bar

--- js/index.js
+++ js/index.js
@@ -4,29 +4,31 @@ $(function() {
   function fillData() {
     $.getJSON("/data/data.json", function(data) {
       var template = $("#event-template");
-      function createEventDiv(e) {
-        var t = template.clone().removeClass("template");
-        t.find(".event-title").text(e.title);
-        t.find(".event-image").append($("<img>", { src: e.image }));
-        t.find(".event-description").text(e.description);
-        return t;
+      function fillEachEvent(array, targetSelector) {
+        for (var i = 0; i < array.length; ++i) {
+          var e = array[i];
+          var t = template.clone().removeClass("invisible");
+          t.find(".event-title").text(e.title);
+          t.find(".event-image").append($("<img>", { src: e.image }));
+          t.find(".event-description").text(e.description);
+          $(targetSelector).append(t);
+        }
       }
-      // "about" data
-      for (var i = 0; i < data.about.events.length; ++i) {
-        var e = data.about.events[i];
-        var t = createEventDiv(e);
-        $(".content-about").append(t);
-      }
-      // "projects" data
-      for (var i = 0; i < data.projects.length; ++i) {
-        var e = data.projects[i];
-        var t = createEventDiv(e);
-        $(".content-projects").append(t);
-      }
+      fillEachEvent(data.about.events, "#content-about");
+      fillEachEvent(data.projects, "#content-projects");
+    });
+  }
+
+  // event handler of navigation bar
+  function navHandler() {
+    $("#nav-about").click(function(e) {
+    });
+    $("#nav-projects").click(function(e) {
     });
   }
 
   // main function
   fillData();
+  navHandler();
 
 });
